158f6a538bc1b0d847aa1c9c7934c469155d0890
charleslin4 <66393454+charleslin4@users.noreply.github.com>
2020-07-16 14:09:13 -0700

    added toHexString function

--- Superworld_token.sol
+++ Superworld_token.sol
@@ -9,7 +9,6 @@ pragma solidity ^0.6.0;
 import "https://github.com/kole-swapnil/openzepkole/token/ERC721/ERC721.sol";
 import "https://github.com/kole-swapnil/openzepkole/access/Ownable.sol";
 
-
 abstract contract ERC20Interface {
     function transferFrom(
         address from,
@@ -531,8 +530,36 @@ contract SuperWorldToken is ERC721, Ownable {
         uint256 balance = address(this).balance;
         (msg.sender).transfer(balance);
     }
+    
+    function toHexString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT license
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 16;
+        }
+        bytes memory buffer = new bytes(digits);
+        uint256 index = digits - 1;
+        temp = value;
+        while (temp != 0) {
+            uint256 digit = temp % 16;
+            if (digit < 10) {
+                buffer[index--] = byte(uint8(48 + digit));
+            } else {
+                buffer[index--] = byte(uint8(87 + digit));
+            }
+            temp /= 16;
+        }
+        return string(buffer);
+    }
+    
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-       string memory x = string(abi.encodePacked('http://geo.superworldapp.com/api/json/metadata/get/tokenId=',tokenId,'&blockchain=e&networkId=4'));
-       return x;
+        string memory x = string(abi.encodePacked('http://geo.superworldapp.com/api/json/metadata/get/0x', toHexString(tokenId)));
+        return x;
     }
 }
