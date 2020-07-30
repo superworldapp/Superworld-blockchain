5da64cf7c17254550daf324258388261966b97ca
Charles Lin <66393454+charleslin4@users.noreply.github.com>
2020-07-30 12:12:48 -0700

    merged from commented version

--- Superworld_coin.sol
+++ Superworld_coin.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.6.0;
 
-import "./github/kole-swapnil/openzepkole/token/ERC20/ERC20.sol"; // for remix
+import "https://github.com/kole-swapnil/openzepkole/token/ERC20/ERC20.sol";
+// import "./github/kole-swapnil/openzepkole/token/ERC20/ERC20.sol"; // for remix
 
 //abstract contract ReceiveApprovalInterface {
 //  function receiveApproval(address buyer, uint256 _value, address _coinAddress, bytes32 _data) public virtual returns (bool success);
 //}
 
+// SuperWorldCoins inherits ERC20
 contract SuperWorldCoins is ERC20 {
     //string name = 'SuperWorldCoin';
     //string symbol = 'SUPERWORLD';
@@ -19,7 +21,9 @@ contract SuperWorldCoins is ERC20 {
         emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
     }
 
-
+    // @dev coverts the string to a 32 bit object
+    // @param takes in a stringToBytes32
+    // @return returns nothing, creates a new bytes object from the string
     function stringToBytes32(string memory source) private pure returns (bytes32 result) {
         bytes memory tempEmptyStringTest = bytes(source);
         if (tempEmptyStringTest.length == 0) {
@@ -31,6 +35,9 @@ contract SuperWorldCoins is ERC20 {
         }
     }
   
+    // @dev creates a new spender object to check if this is the appropriate transaction and person performing transaction
+    // @param takes in one address of a payer and the amount they are paying
+    // @return gives a boolean, true if the transaction was approved
     function approveAndCall(address _spender, uint256 _value, bytes32 data) public returns (bool) {
         //SuperWorldToken.receiveApproval(address buyer, uint coins, address _coinAddress, bytes32 _data)
         bool isSuccess = false;
