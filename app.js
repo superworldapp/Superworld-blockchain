5af8735447e9ea79180e5e927a3746d2686b2f8d
kole-swapnil <swapnilkole7500@gmail.com>
2020-08-03 17:26:28 +0530

    environment var added

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-//import * as Web3 from 'web3'
+
+require("dotenv").config();
 const web3 = require('web3')
 //import { OpenSeaPort, Network } from 'opensea-js'
 const openSeas = require('opensea-js')
-
+const INFURA_KEY = process.env.INFURA_KEY;
+const MNEMONIC = process.env.MNEMONIC;
 //for creating read only transactions
-const provider = new web3.providers.HttpProvider('https://mainnet.infura.io')
+console.log(INFURA_KEY);
+console.log(MNEMONIC);
+const provider = new web3.providers.HttpProvider('https://rinkeby.infura.io/v3/${INFURA_KEY}')
 
 const seaport = new openSeas.OpenSeaPort(provider, {
-    networkName: openSeas.Network.Main
+    networkName: openSeas.Network.Rinkeby
 })
 
 console.log(seaport)
--- app.ts
+++ app.ts
@@ -1,15 +1,14 @@
-import * as Web3 from 'web3'
+import * as web3 from 'web3'
 import { OpenSeaPort, Network } from 'opensea-js'
  
 // This example provider won't let you make transactions, only read-only calls:
-const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io')
+const provider = new web3.providers.HttpProvider('https://rinkeby.infura.io/v3/${INFURA_KEY}')
  
 const seaport = new OpenSeaPort(provider, {
-  networkName: Network.Main
+  networkName: Network.Rinkeby
 })
 
 console.log(seaport)
-
 //have to keep an infura key on the environment variable
 //use environment vairable require('dotenv').config();
 //env vars: process.env.INFURA_KEY
